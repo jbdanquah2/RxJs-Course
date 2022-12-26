39e342a387d19d1c9b8b37019e0bb617885ffa59
John Danquah-Boateng <yjbdanquah@gmail.com>
2022-12-26 12:21:17 +0000

    init

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { concat, interval, merge, noop, Observable, of, timer } from "rxjs";
+import { map } from "rxjs/operators";
+import { createHttpObservable } from "../common/util";
 
 @Component({
-  selector: 'about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    const interval$ = interval(1000);
+    const interval2$ = interval$.pipe(map((val) => 10 * val));
+    const results = merge(interval$, interval2$);
+    results.subscribe(console.log);
   }
-
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,44 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {interval, Observable, of, timer} from 'rxjs';
-import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
-
+import { Component, OnInit } from "@angular/core";
+import { Course } from "../model/course";
+import { interval, noop, Observable, of, timer } from "rxjs";
+import {
+  catchError,
+  delayWhen,
+  map,
+  retryWhen,
+  shareReplay,
+  tap,
+} from "rxjs/operators";
+import { createHttpObservable } from "../common/util";
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: "home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
+  biginnerCourses$: Observable<Course[]>;
+  advancedCourses$: Observable<Course[]>;
 
+  constructor() {}
 
-    constructor() {
+  ngOnInit() {
+    const http$ = createHttpObservable("api/courses");
+    const courses$: Observable<Course[]> = http$.pipe(
+      tap(() => console.log("HTTPS request executed")),
+      map((res) => res["payload"]),
+      shareReplay()
+    );
 
-    }
-
-    ngOnInit() {
-
-
-
-    }
-
+    this.biginnerCourses$ = courses$.pipe(
+      map((courses) =>
+        courses.filter((course) => course.category == "BEGINNER")
+      )
+    );
+    this.advancedCourses$ = courses$.pipe(
+      map((courses) =>
+        courses.filter((course) => course.category == "ADVANCED")
+      )
+    );
+  }
 }
